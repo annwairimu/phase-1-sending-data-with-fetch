dca38ac7c7b549fb05fe423241de625444da7b42
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-03-31 00:21:56 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = { name, email };
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML = data.id;
+      })
+      .catch(error => {
+        document.body.innerHTML = error.message;
+      });
+  }
